03831128eb563637fded45b11ccb2a1efec488cb
João Manoel <joaomanoel7@gmail.com>
2016-10-17 05:01:17 -0200

    update repo ...

--- 2016-2/Estrutura de Dados I/aluno_01.c
+++ 2016-2/Estrutura de Dados I/aluno_01.c
@@ -1,34 +1,29 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-struct Aluno {
+typedef struct {
     int id;
     char nome[30];
     char sexo;
     int idade;
-};
+} Aluno;
 
 int main(void) {
 
+    Aluno a[3];
     int i;
-    char *nome;
-    struct Aluno a[3];
+    char format[30];
 
-    for (i=0; i<3; ++i) {
-        nome = (char *) malloc(30 * sizeof(char) );
-        printf("Informe os dados do aluno %i:\n\n", i+1);
+    for (i=0; i<3; i++) {
+        printf("\nInforme os dados do aluno %d:\n\n", i+1);
         a[i].id=i+1;
-        printf("Nome:\n");
-        scanf("%[^\n]s\n", nome);
-        printf("Nome = \"%s\"\n", nome);
-        strcpy(a[i].nome, nome);
-        free(nome);
-        printf("Sexo:\n");
-        scanf("%c\n", &a[i].sexo);
-        printf("Sexo = \"%c\"\n", a[i].sexo);
-        printf("Idade:\n");
-        scanf("%i\n", &a[i].idade);
+        printf("Nome: ");
+        scanf(" %[^\n]30", a[i].nome);
+        printf("Sexo: ");
+        scanf(" %c", &(a[i].sexo));
+        printf("Idade: ");
+        scanf(" %d", &(a[i].idade));
     }
 
     system("cls || clear");
@@ -37,19 +32,20 @@ int main(void) {
     do {
         printf("PESQUISA DE ALUNOS:\n\n");
         printf("PS: para sair informe o id=5\n\n");
-        printf("Informe a posicao do aluno (id):\n");
-        scanf("%i\n", &i);
+        printf("Informe a posicao do aluno (id): ");
+        scanf(" %d", &i);
         if ( ( i<1 || i>3 ) && i!=5 ) {
-            printf("O Id digitado (%i) eh invalido!\n", i);
-            system("pause || read");
+            printf("\nO Id digitado (%d) eh invalido!\n", i);
+            system("pause || read teste");
             system("cls || clear");
             continue;
         }
         if (i!=5) {
-            printf("%s %s %40s %s\n", "Id", "Nome", "Sexo", "Idade");
-            printf("%i %s %40s %s\n", a[i].id, a[i].nome, a[i].sexo, a[i].idade);
+            printf("\n%s %s %40s %s\n", "Id", "Nome", "Sexo", "Idade");
+            sprintf(format, "%%2d %%s %%%zdc %%5d\n", 40 - strlen(a[i-1].nome) + 4);
+            printf(format, a[i-1].id, a[i-1].nome, a[i-1].sexo, a[i-1].idade);
             printf("\nPressione <Enter> para continuar.\n");
-            system("pause || read");
+            system("pause || read teste");
             system("cls || clear");
         }
     } while (i!=5);
