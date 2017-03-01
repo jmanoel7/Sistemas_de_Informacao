8020b61708e3ee4e44a3bb2b5018d698af10d1a7
João Manoel <joaomanoel7@gmail.com>
2017-03-01 06:54:18 -0300

    update repo ...

---
+++ 2016-2/Estrutura de Dados I/codigo_c/lista-01/ex_05.h
@@ -0,0 +1,51 @@
+/*
+ * Aluno:       João Manoel Leite Ribeiro Nogueira
+ * Matrícula:   20131011090230
+ * Lista:       01
+ * Exercício:   01
+ * Disciplina:  Estrutura de Dados I
+ * Curso:       Bacharelado em Sistemas de Informação
+ * Professor:   Dory Gonzaga Rodrigues
+ * IFG:         Câmpus Goiânia
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (void) {
+
+    int i;
+    char palavra[5], teste1[5], teste2[5];
+
+    do {
+
+        printf("Digite uma palavra com 5 letras:\n");
+        scanf("%s", palavra);
+
+        if (strlen(palavra) == 5)
+            break;
+
+        printf("\nERRO: a palavra informada (%s) não possui 5 letras!!!\n\nInforme-a novamente!!!\n\n", palavra);
+
+    } while (1);
+
+    printf("\nA palavra da: ESQUERDA  para: DIREITA  -> ");
+    for (i=0; i<=4; ++i) {
+        printf("%c", palavra[i]);
+        teste1[i] = palavra[i];
+    }
+
+    printf("\nA palavra da: DIREITA   para: ESQUERDA -> ");
+    for (i=4; i>=0; --i) {
+        printf("%c", palavra[i]);
+        teste2[4-i] = palavra[i];
+    }
+
+    if (strcmp(teste1, teste2) == 0)
+        printf("\n\nA palavra informada (%s) é palíndroma!\n", palavra);
+    else
+        printf("\n\nA palavra informada (%s) NÃO é palíndroma!\n", palavra);
+
+    return 0;
+}
---
+++ 2016-2/Estrutura de Dados I/codigo_c/lista-01/main.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2017 João Manoel <joao.manoel@academico.ifg.edu.br>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ex_01.h"
+
+int main(void) {
+  return Exercicio_01();
+}
